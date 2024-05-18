689da47fd1b75762bc4e551f3f84c9f16630699f
Vincent Tran <tranvincent73@gmail.com>
2024-05-17 22:11:59 -0500

    Added machine model & other datasets

--- machineLearning.py
+++ machineLearning.py
@@ -1,15 +1,19 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import classification_report
 import plotly.express as px
+import streamlit as st
 
-# Define transaction categories and corresponding keywords
+
+# manual categorization of transactions for training the model
 categories = {
-    "Food": ["TAQUERIA TAXCO", "IHOP", "ORACL*WAFFLE HOUS", "85C BAKERY CAFE", "DAVES HOT CHICKEN", "TACO BELL", "BA LE RESTAURANT", "PIRANHA KILLER", "HAPPY LEMON", "CHICKEN EXPRESS", "SQ *KONA ICE", "BON K BBQ", "NINJARAMENANDROYA", "WHATABURGER", "RAISING CANES", "PORT OF SUBS", "MCDONALD'S", "CHIPOTLE", "TOUCH OF TOAST", "PANDA EXPRESS", "CHIPOTLE", "DINGTEA", "WICKED SNOW", "WINGSTOP", "PANDA EXPRESS", "CHICK-FIL-A", "210 BRAUMS STORE", "10402 CAVA CHAMPI"],
-    "Shopping": ["eBayCommerce", "Alibaba.com", "APPLE", "Amazon.com", "Nike.com", "SP WATC STUDIO", "EDFINITY.COM", "MUSINSA", "WL *STEAM", "Zara.com", "UTA BOOKSTORE", "WAL Wal-Mart", "WM SUPERCENTER", "TARGET", "BEST BUY", "APPLE.COM/BILL", "Nike.com", "Birkenstock", "TARGET", "AMZN DIGITAL", "AMZN Mktp"],
-    "Transportation": ["FRONTIER", "QT", "MURPHY EXPRESS", "7-ELEVEN", "Upside", "UTA PARK TRANS"],
-    "Finance": ["GOLDMAN SACHS", "Cash App", "DISCOVER", "TRANSFER", "BKOFAMERICA", "UTA ARL MYMAV", "UT ARLINGTON"],
-    "Misc": ["Zelle payment", "EXPRT TA"]
+    "Food": ["FOODS", "EATS", "TACO DINER", "NICK & SAM'S", "LOCHLANDS", "WHISKEY CAKE", "IN-N-OUT BURGER", "GRAND LUX CAFE", "THE RUSTIC", "EL FENIX", "CANTINA LAREDO", "FOGO DE CHAO", "P.F. CHANG'S", "CHICKEN N PICKLE", "BURGER KING", "SUBWAY", "STARBUCKS", "BLUE BOTTLE COFFEE", "OLIVE GARDEN", "KFC", "DUNKIN' DONUTS", "PEET'S COFFEE", "TAQUERIA TAXCO", "IHOP", "ORACL*WAFFLE HOUS", "85C BAKERY CAFE", "DAVES HOT CHICKEN", "TACO BELL", "BA LE RESTAURANT", "PIRANHA KILLER", "HAPPY LEMON", "CHICKEN EXPRESS", "SQ *KONA ICE", "BON K BBQ", "NINJARAMENANDROYA", "WHATABURGER", "RAISING CANES", "PORT OF SUBS", "MCDONALD'S", "CHIPOTLE", "TOUCH OF TOAST", "PANDA EXPRESS", "CHIPOTLE", "DINGTEA", "WICKED SNOW", "WINGSTOP", "PANDA EXPRESS", "CHICK-FIL-A", "210 BRAUMS STORE", "10402 CAVA CHAMPI"],
+    "Shopping": ["CUT", "HAIRCUT", "CHINGONCUT", "NEIMAN MARCUS", "LOUIS VUITTON", "GALLERIA", "WILLIAMS SONOMA", "THE SHOPS AT LEGACY", "THE NORTH FACE", "DICK'S SPORTING GOODS", "SPOTIFY", "URBAN OUTFITTERS", "GAP", "NORDSTROM", "PEETS COFFEE", "ASOS", "H&M", "NETFLIX.COM", "HOME DEPOT", "COSTCO", "PRIME VIDEO", "KROGER", "HULU.COM", "WALGREENS", "LOWE'S", "EBAY", "eBayCommerce", "Alibaba.com", "APPLE", "Amazon.com", "Nike.com", "SP WATC STUDIO", "EDFINITY.COM", "MUSINSA", "WL *STEAM", "Zara.com", "UTA BOOKSTORE", "WAL Wal-Mart", "WM SUPERCENTER", "TARGET", "BEST BUY", "APPLE.COM/BILL", "Nike.com", "Birkenstock", "TARGET", "AMZN DIGITAL", "AMZN Mktp"],
+    "Transportation": ["GAS", "CAR", "BUC-EE'S", "UBER RIDE", "LYFT RIDE", "FRONTIER", "QT", "MURPHY EXPRESS", "7-ELEVEN", "Upside", "UTA PARK TRANS"],
+    "Finance": ["VENMO", "PAYPAL", "CHASE BANK", "GOLDMAN SACHS", "Cash App", "DISCOVER", "TRANSFER", "BKOFAMERICA", "UTA ARL MYMAV", "UT ARLINGTON"],
+    "Misc": ["Zelle payment", "EXPRT TA", "Beginning balance"]
 }
 
 def categorize_transaction(description):
@@ -19,15 +23,49 @@ def categorize_transaction(description):
                 return category
     return "Misc"
 
+def machineLearning(data, data2):
+    # Training data
+    X_train = data['Description']
+    y_train = data['Category']
+    X_test = data2['Description']
+
+    # Create a pipeline with TF-IDF vectorizer and Naive Bayes classifier
+    model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+
+    # Train the model
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    data2['Category'] = y_pred
+
+    # Print classification report
+    print(classification_report(y_train, model.predict(X_train)))
 
 def main():
+    # Add categorization to the data for testing model
+    data = pd.read_csv("Data/testData.csv", skiprows=6)
+    df = pd.DataFrame(data, columns=["Date", "Description", "Amount", "Running Bal."])
+
+    df['Category'] = df['Description'].apply(categorize_transaction)
+    df['Amount'] = df['Amount'].replace({',': ''}, regex=True).astype(float)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Running Bal.'] = df['Running Bal.'].replace({',': ''}, regex=True).astype(float)
+
+    # Save the categorized data to a new CSV file (optional)
+    df.to_csv("Data/testCategorizedData.csv", index=False)
+
+    data = pd.read_csv("Data/testCategorizedData.csv")
+
+
+
+
     st.set_page_config(page_title="Finance Dashboard", layout='wide')
 
     with st.sidebar:
         # create two buttons for the user to select 1. Dashboard 2. Future Output
         st.title('Navigation :heavy_exclamation_mark:')
         page = st.selectbox('Go to :car:', ['Finance Dashboard', 'Future Finance'])
-
 
     st.title('Finance Dashboard  :chart_with_upwards_trend:')
 
@@ -36,14 +74,14 @@ def main():
     if file is not None:
         df = pd.read_csv(file, skiprows=6)
 
+        machineLearning(data, df)
         # Apply categorization function to the Description column
-        df['Category'] = df['Description'].apply(categorize_transaction)
         df['Amount'] = df['Amount'].replace({',': ''}, regex=True).astype(float)
         df['Date'] = pd.to_datetime(df['Date'])
         df['Running Bal.'] = df['Running Bal.'].replace({',': ''}, regex=True).astype(float)
 
         st.subheader('Raw Data')
-        st.dataframe(df, use_container_width=True)
+        st.dataframe(df.iloc[1:], use_container_width=True)
 
         col1, col2 = st.columns(2)
 
@@ -53,12 +91,11 @@ def main():
                 df,
                 x='Date',
                 y='Running Bal.',
-                color_discrete_sequence=px.colors.qualitative.Safe  ,
+                color_discrete_sequence=px.colors.qualitative.Safe,
             )
 
             st.subheader('Total Running Balance')
             st.plotly_chart(fig, use_container_width=True, height=400)
-
 
         with col2:
             transaction_freq_by_month = df.groupby(df['Date'].dt.to_period('M')).size()
@@ -76,10 +113,9 @@ def main():
             st.subheader('Transaction Frequency')
             st.plotly_chart(fig, use_container_width=True, height=400)
 
-        
         st.markdown("---")
 
-        col1, col2 = st.columns([0.4,0.5])
+        col1, col2 = st.columns([0.4, 0.5])
 
         with col1:
             # Plotting a pie chart of the spending categories using Plotly
@@ -110,7 +146,6 @@ def main():
 
                 for key in category_running_totals.keys():
                     category_running_totals[key].append(running_totals[key])
-
 
             df_running_totals = pd.DataFrame(category_running_totals)
             fig = px.line(
